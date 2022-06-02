c4387e98129419d3612404d45e47dac0ebcf916d
Leo Maza <leomaza2222@gmail.com>
2022-06-02 09:34:56 -0300

    Primer reto quincenal V 1.0

---
+++ retos.js
@@ -0,0 +1,81 @@
+//problema 1
+let manzanasMaria = 5;
+let manzanasPedro = 8;
+let manzanasSusana = 9;
+
+console.log(manzanasMaria + manzanasPedro + manzanasSusana)
+
+
+//Problema 2
+let magdalenasTotalJuan = 28;
+let amigosJuan = 12;
+let magdalenaJuan = 1;
+let totalMagdalenas = (magdalenasTotalJuan - magdalenaJuan) - amigosJuan;
+
+console.log("En total quedan " + totalMagdalenas + " magdalenas.")
+
+//Problema 3
+let hamburguesa = 35;
+let refresco = 18;
+let totalSum = hamburguesa + refresco;
+console.log("Paco pago en total $" + totalSum)
+
+//Problema 4 
+let galletas = 5;
+let cajas = 4;
+let totalGalletas = galletas * cajas;
+
+console.log("El total de galletas es de " + totalGalletas);
+
+//Problema 5 
+let kilomerosConduc = 2.6;
+let kmAMetros = 1000;
+let metrosCaminados = 691;
+let kmTotales = (kmAMetros * kilomerosConduc) + metrosCaminados;
+let milimetros = 1000;
+let mmTotales = kmTotales * milimetros;
+
+console.log("La distancia total recorrida en Milimetros es de " + mmTotales);
+
+//Problema 6
+let ingredienteA = "ajo";
+let ingredienteB = "manteca";
+let ingredienteC = "pan";
+let ingredienteD = "sal";
+let ingredienteE = "perejil";
+let ingredienteF = "Queso parmesano";
+
+console.log("1 Precalienta el horno a 220°C.Forrar una bandeja de horno grande con papel aluminio / 2 en un tazon pequeño cree la mezcla de " + ingredienteB + " con el " + ingredienteA + " la " + ingredienteD + " , y el " + ingredienteE, " hasta que esten bien combinados. / 3 Extender la mezcla de la " + ingredienteB + " sobre las rebanadas de " + ingredienteC + ". Espolvorear con " + ingredienteF + ". / 4 Hornea el " + ingredienteC + " hasta que este tostado en los bordes (unos 10 minutos). Servir y disfrutar")
+
+//Problema 7 ingredientes en Variables
+
+let huevo = 1;
+let cucharaDeAceite = 2;
+let mlTazaDeLeche = 750;
+let salPizca = 1;
+let tazaDeHarina = 1;
+let cucharaPolvoDeHornear = 1;
+
+
+
+
+
+//Problema 8 identificar principales variables
+
+let aniosDescomposicionBolsa = 200;
+let materialBolsa = "Plastico";
+let animalesAfectados = ["Tortugas", "Delfines", "Focas"];
+
+
+
+
+
+
+
+//Problema 9
+var velocidadInicial = "No esta definida"
+var velocidadTotal = 60;
+var aceleracion = 20;
+var tiempo = velocidadTotal / aceleracion;
+
+console.log("El tiempo que se demora en alcanzar 60 km/h es:" + tiempo + " horas");
